76284c7bc2f951ab5a3661e8bd3a330a9df399fa
lsmman <tmdgus507@naver.com>
2019-06-03 11:18:47 +0900

    괄호의 합 맞았다

--- python/2504_괄호의값.py
+++ python/2504_괄호의값.py
@@ -20,40 +20,47 @@ stack = Stack()
 mul_depth = 1
 total_sum = 0
 impossible = 0
+s = 0
+l = 0
 leng = len(input_data)
 
 for idx in range(leng):
-  brk = input_data[idx]
-  if brk.__eq__("("):
-    mul_depth = mul_depth * 2
-    stack.push(brk)
-  elif brk.__eq__("["):
-    mul_depth = mul_depth * 3
-    stack.push(brk)
-  elif brk.__eq__(")"):
-    if input_data[idx-1].__eq__('('):
-        total_sum += mul_depth
-    if stack.peek().__eq__("("):
+    brk = input_data[idx]
+    if brk.__eq__("("):
+        s += 1
+        mul_depth = mul_depth * 2
+        stack.push(brk)
+
+    elif brk.__eq__("["):
+        l += 1
+        mul_depth = mul_depth * 3
+        stack.push(brk)
+
+    elif brk.__eq__(")"):
+        s -= 1
+        if not stack.peek().__eq__("("):
+            impossible = 1
+            break
+
+        if input_data[idx-1].__eq__('('):
+            total_sum += mul_depth
+        
         mul_depth = mul_depth / 2
         stack.pop()
-    else : 
-        impossible = 1
-        break
-  elif brk.__eq__("]"):
-    if input_data[idx-1].__eq__('['):
-        total_sum += mul_depth
-    if stack.peek().__eq__("["):
+
+    elif brk.__eq__("]"):
+        l -= 1
+        if not stack.peek().__eq__("["):
+            impossible = 1
+            break
+
+        if input_data[idx-1].__eq__('['):
+            total_sum += mul_depth
+        
         mul_depth = mul_depth / 3
         stack.pop()
-    else : 
-        impossible = 1
-        break
-if (stack.is_empty == 0) or impossible:
+
+if (stack.is_empty == 0) or impossible or l or s:
     print(0)
 else :
     print(int(total_sum))
-
-  
-
-
-# 2*(2+3*3)+2*3
