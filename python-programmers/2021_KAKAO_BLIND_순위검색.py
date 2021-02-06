3b89989737e3a1d1dbd63fc8d0e58dc2005ea045
lsmman <tmdgus507@naver.com>
2021-02-07 01:55:37 +0900

    done 순위검색

--- python-programmers/2021_KAKAO_BLIND_순위검색.py
+++ python-programmers/2021_KAKAO_BLIND_순위검색.py
@@ -1,26 +1,31 @@
 from collections import defaultdict
-import bisect
+from bisect import insort, bisect_left
 
 
 def solution(info, queries):
-    ans = [0] * len(queries)
+    ans = []
     sets = defaultdict(list)
-    for ele in info:
-        *cur, score = ele.split()
+    for _info in info:
+        *data, score = _info.split()
         for k in range(16):
             key = ""
             for sift in range(4):
                 if k & (1 << sift):
                     key += "-"
                 else:
-                    key += cur[sift][:2]
-            bisect.insort(sets[key], int(score))
+                    key += data[sift][0]
+            insort(sets[key], int(score))
 
-    for q_i, query in enumerate(queries):
-        *cur_q, q_score = query.split()
+    for q in queries:
+        *cur_q, q_score = q.split()
         key = ""
         for c in cur_q:
             if c != "and":
-                key += c[:2]
-        ans[q_i] = len(sets[key]) - bisect.bisect_left(sets[key], int(q_score))
+                key += c[0]
+        count = len(sets[key]) - bisect_left(sets[key], int(q_score))
+        ans.append(count)
     return ans
+
+
+# https://velog.io/@study-dev347/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%88%9C%EC%9C%84-%EA%B2%80%EC%83%89
+# https://lioliolio.github.io/python-bisect-module/
