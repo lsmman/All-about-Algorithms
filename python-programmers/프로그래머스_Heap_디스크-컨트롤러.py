c2846d9a644ea8db360eb7c2c5a430e814b016c1
lsmman <tmdgus507@naver.com>
2021-06-05 23:35:27 +0900

    solve 디스크컨트롤러 but need refactoring

---
+++ python-programmers/프로그래머스_Heap_디스크-컨트롤러.py
@@ -0,0 +1,23 @@
+def solution(jobs):
+    def get_time_taken(jobs):
+        answer = 0
+        cur = 0
+        done = [False for _ in range(len(jobs))]
+        job = len(jobs)
+        while job:
+            for i, (srt, dur) in enumerate(jobs):
+                if not done[i] and srt <= cur:
+                    print(cur, cur + dur - srt, srt, dur)
+                    done[i] = True
+                    cur = dur + cur
+                    answer += cur - srt
+                    job -= 1
+                    break
+        return answer // len(jobs)
+
+    jobs.sort(key=lambda x: x[0])
+    fcfs = get_time_taken(jobs)
+    jobs.sort(key=lambda x: x[1])
+    sjf = get_time_taken(jobs)
+
+    return min(fcfs, sjf)
