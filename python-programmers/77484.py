424e5d562cb0454841b54eefaaa423eb9aacfab2
lsmman <tmdgus507@naver.com>
2021-06-10 01:00:47 +0900

    solve 77484

--- python-programmers/77484.py
+++ python-programmers/77484.py
@@ -3,43 +3,35 @@
 # 문제 링크 : https://programmers.co.kr/learn/courses/30/lessons/77484
 # 문제 걸린 시간 : 5분
 
+"""
+문제 설명 : 
+1부터 45 사이에 숫자를 맞추는 로또가 있습니다. 숫자가 일치한 수에 따라 아래 표와 같이 순위가 결정됩니다. 
+로또를 구매한 민우는 당첨 번호 발표일을 학수고대하고 있었습니다. 하지만, 민우의 동생이 로또에 낙서를 하여, 일부 번호를 알아볼 수 없게 되었습니다. 당첨 번호 발표 후, 민우는 자신이 구매했던 로또로 당첨이 가능했던 최고 순위와 최저 순위를 알아보고 싶어 졌습니다.
+알아볼 수 없는 번호를 0으로 표기하기로 하고, 민우가 구매한 로또 번호 6개가 44, 1, 0, 0, 31 25라고 가정해보겠습니다. 
+당첨 번호 6개가 31, 10, 45, 1, 6, 19라면 당첨 가능한 최고 순위는 다음과 같습니다.
+최고 순위는 31, 1번이 일치하고 0의 자리에 모두 당첨 번호가 들어가면 4개가 일치하므로 3등
+최저 순위는 31, 1이 일치하여 2개가 일치하므로 5등
 
-def solution(rows, columns, queries):
-    answer = []
-    MAP = [[r * columns + c + 1 for c in range(columns)] for r in range(rows)]
+|순위 | 당첨 내용|
+|1	|6개 번호가 모두 일치|
+|2	|5개 번호가 일치|
+|3	|4개 번호가 일치|
+|4	|3개 번호가 일치|
+|5	|2개 번호가 일치|
+|6(낙첨) |그 외|
+"""
 
-    for loc in queries:
-        y1, x1, y2, x2 = list(map(lambda x: x - 1, loc))
-        temp = preq = MAP[y1][x1]
-        min_val = rows * columns + 1
 
-        y = y1
-        for x in range(x1, x2):
-            temp = preq
-            preq = MAP[y][x]
-            MAP[y][x] = temp
-            min_val = min(preq, min_val)
+def solution(lottos, win_nums):
+    unknown = 0  # 지워져 값을 모르는 번호의 수
+    win = 0  # 일치한 번호의 수
+    rank = [6, 6, 5, 4, 3, 2, 1]  # index, value = 일치한 개수, 등수
+    for l in lottos:
+        if l == 0:  # 값이 지워진 경우
+            unknown += 1
+        elif l in win_nums:  # 번호가 당첨 번호 중 하나 일 때 = 일치할 때
+            win += 1
 
-        x = x2
-        for y in range(y1, y2):
-            temp = preq
-            preq = MAP[y][x]
-            MAP[y][x] = temp
-            min_val = min(preq, min_val)
-
-        y = y2
-        for x in range(x2, x1, -1):
-            temp = preq
-            preq = MAP[y][x]
-            MAP[y][x] = temp
-            min_val = min(preq, min_val)
-
-        x = x1
-        for y in range(y2, y1 - 1, -1):
-            temp = preq
-            preq = MAP[y][x]
-            MAP[y][x] = temp
-            min_val = min(preq, min_val)
-
-        answer.append(min_val)
-    return answer
+    # 최선의 경우 : 밝혀진 값 중 일치한 번호와 값을 모르는 번호가 모두 당첨 번호일 때
+    # 최악의 경우 : 모르는 번호는 모두 틀려서, 밝혀진 값 중 일치한 번호만 당첨 번호일 때
+    return [rank[unknown + win], rank[win]]
--- python-programmers/77485.py
+++ python-programmers/77485.py
@@ -1,44 +1,20 @@
-# 행렬 테두리 회전하기
-# 문제 링크 : https://programmers.co.kr/learn/courses/30/lessons/77485?language=python3
-# 문제 걸린 시간 : 36분
+# 다단계 칫솔 판매
+# 문제 링크 : https://programmers.co.kr/learn/courses/30/lessons/77486
+# 문제 걸린 시간 : 25분
 
 
-def solution(rows, columns, queries):
-    answer = []
-    MAP = [[r * columns + c + 1 for c in range(columns)] for r in range(rows)]
+def solution(enroll, referral, seller, amount):
+    profits = [0] * len(enroll)
+    name2idx = {name: idx for idx, name in enumerate(enroll)}
+    name2idx["-"] = -1
+    referral = list(map(lambda x: name2idx[x], referral))
 
-    for loc in queries:
-        y1, x1, y2, x2 = list(map(lambda x: x - 1, loc))
-        temp = preq = MAP[y1][x1]
-        min_val = rows * columns + 1
-
-        y = y1
-        for x in range(x1, x2):
-            temp = preq
-            preq = MAP[y][x]
-            MAP[y][x] = temp
-            min_val = min(preq, min_val)
-
-        x = x2
-        for y in range(y1, y2):
-            temp = preq
-            preq = MAP[y][x]
-            MAP[y][x] = temp
-            min_val = min(preq, min_val)
-
-        y = y2
-        for x in range(x2, x1, -1):
-            temp = preq
-            preq = MAP[y][x]
-            MAP[y][x] = temp
-            min_val = min(preq, min_val)
-
-        x = x1
-        for y in range(y2, y1 - 1, -1):
-            temp = preq
-            preq = MAP[y][x]
-            MAP[y][x] = temp
-            min_val = min(preq, min_val)
-
-        answer.append(min_val)
-    return answer
+    for name, profit in zip(seller, amount):
+        idx = name2idx[name]
+        profit = profit * 100
+        while idx != -1 and profit:
+            referral_profit = int(profit * 0.1)
+            profits[idx] = profits[idx] + (profit - referral_profit)
+            profit = referral_profit
+            idx = referral[idx]
+    return profits
