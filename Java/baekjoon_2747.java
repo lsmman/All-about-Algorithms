0cc381c40aad8c23ed62cec021f3682a6bad1b0f
lsmman <lsmman07@gmail.com>
2021-10-07 19:59:14 +0900

    solve 2747

---
+++ Java/baekjoon_2747.java
@@ -0,0 +1,30 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        int n = Integer.parseInt(s);
+
+        if (n <= 1) {
+            System.out.println(n);
+            return;
+        }
+        int pre, next;
+        pre = 0;
+        next = 1;
+        int result = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            result = pre + next;
+            pre = next;
+            next = result;
+        }
+
+        System.out.println(result);
+    }
+}
