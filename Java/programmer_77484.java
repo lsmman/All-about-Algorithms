817cad139e17544c8a6408e7df9f474cac548a18
lsmman <lsmman07@gmail.com>
2021-11-02 11:57:05 +0900

    remove un-necessary comment and numbers

--- Java/programmer_77484.java
+++ Java/programmer_77484.java
@@ -1,28 +1,3 @@
-
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
         int[] ranks = new int[] {6, 6, 5, 4, 3, 2, 1};
